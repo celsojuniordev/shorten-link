aa2b26d1b3eb259f14ed112e60f288ab0113e2df
Celso Junior <celsoestercio.jr@gmail.com>
2022-01-03 23:37:44 -0300

    first commit

---
+++ src/main/groovy/com/shorten/link/LinkApplication.groovy
@@ -0,0 +1,13 @@
+package com.shorten.link
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LinkApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LinkApplication, args)
+	}
+
+}
